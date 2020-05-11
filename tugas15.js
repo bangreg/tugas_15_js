2f265ea97d7529197abc2a38394d249fbee70837
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:15:11 +0700

    Tugas 15 JS

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+function sisiMiring(){
+  var AB = 8; console.log('Nilai AB : ', AB)
+  var BC = 6; console.log('Nilai BC : ', BC)
+  var AC = Math.sqrt(Math.pow(AB,2)+Math.pow(BC,2));
+  console.log('panjang sisi AC pada segitiga siku-siku tersebut adalah', AC ,'cm');
+}
+sisiMiring()
